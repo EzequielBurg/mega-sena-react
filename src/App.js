4dc5c7cb2c1f80b673a24295fd439ed22254f16a
EzequielBurg <ezekiel_sci@hotmail.com>
2020-12-20 13:53:56 -0300

    Updating react functions and responsive styles

--- src/App.js
+++ src/App.js
@@ -1,94 +1,100 @@
-import React, { useState } from 'react';
-import { Container, ButtonGroup, Button, CircularProgress  } from '@material-ui/core';
+import React, { useCallback, useState } from 'react';
+import { ButtonGroup, Button, CircularProgress  } from '@material-ui/core';
 import './App.css'
 
-export default function App() {
-  const [value, setValue]     = useState(0)
-  const [numbers, setNumbers] = useState([])
+const App = () => {
+  const [value, setValue]     = useState(0);
+  const [numbers, setNumbers] = useState([]);
 
-  const getRandomNumber = () => {
+  const getRandomNumber = useCallback(() => {
     let min = Math.ceil(1);
     let max = Math.floor(60);
     
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  const handleSelector = value => {
-    setNumbers([])
-    setValue(value)
-
-    setTimeout(() => {
-      setValue(0)
-    }, 2000)
-    
-    sortNumbers(value)
-  }
-
-  const sortNumbers = value => {
-    const randomNumbers = []
+  }, []);
+  
+  const sortNumbers = useCallback((value) => {
+    const randomNumbers = [];
 
     for (let i = 1; i <= value; i++) {
-      let newElement = getRandomNumber()
-      while (randomNumbers.indexOf(newElement) !== -1)
-        newElement = getRandomNumber()
+      let newElement = getRandomNumber();
+      while (randomNumbers.indexOf(newElement) !== -1) {
+        newElement = getRandomNumber();
+      }
       
-      randomNumbers.push(newElement)
+      randomNumbers.push(newElement);
     }
 
     setNumbers(randomNumbers.sort((a, b) => a - b))
-  }
+  }, [getRandomNumber]);
+
+  const handleSelector = useCallback((value) => {
+    setNumbers([]);
+    setValue(value);
+
+    setTimeout(() => {
+      setValue(0)
+    }, 2000);
+    
+    sortNumbers(value);
+  }, [sortNumbers]);
 
   return (
     <>
       <div className="title">
         <h1>Geração aleatória de números para a MEGA-SENA</h1>
       </div>
-      <Container>
+      <div className="container">
         <h3>Selecione quantos números deseja sortear:</h3>
-        <ButtonGroup aria-label="outlined secondary button group" color="secondary" size="large">
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(6)}>6</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(7)}>7</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(8)}>8</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(9)}>9</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(10)}>10</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(11)}>11</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(12)}>12</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(13)}>13</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(14)}>14</Button>
-          <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(15)}>15</Button>
-        </ButtonGroup>
-      
-      {
-        value > 0 ?
-        <div className="geral">
-          <div className="text">
-            <h2>GERANDO {value} NÚMEROS ALEATÓRIOS </h2>
-          </div>
-          
-          <div className="loader">
-            <CircularProgress color="primary"/>
-          </div>
-        </div> :
-        <span></span>
-      }
+        <div className="buttom-group">
+          <ButtonGroup color="secondary" size="large">
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(6)}>6</Button>
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(7)}>7</Button>
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(8)}>8</Button>
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(9)}>9</Button>
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(10)}>10</Button>
+          </ButtonGroup>
 
-      {
-        numbers.length > 0 && value === 0 ?
-        <div className="result">
-          <strong>Os números aleatórios gerados são:</strong>
-          <ul>
-            {
-              numbers.map((number, index) => (
-                <div className="li" key={index}>
-                  <li><strong>{number}</strong></li>
-                </div>
-              ))
-            }
-          </ul>
-        </div> :
-        <span></span>
-      }
-      </Container>
+          <ButtonGroup color="secondary" size="large">
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(11)}>11</Button>
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(12)}>12</Button>
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(13)}>13</Button>
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(14)}>14</Button>
+            <Button id="button" variant="contained" color="secondary" onClick={() => handleSelector(15)}>15</Button>
+          </ButtonGroup>
+        </div>
+      
+        {
+          value > 0 ?
+          <div className="geral">
+            <h2>GERANDO {value} NÚMEROS ALEATÓRIOS </h2>
+            <CircularProgress color="primary"/>
+          </div> :
+          <span></span>
+        }
+
+        {
+          numbers.length > 0 && value === 0 ?
+          <div className="result">
+            <strong>Os números aleatórios gerados são:</strong>
+            <ul>
+              {
+                numbers.map((number, index) => (
+                  <div className="li" key={index}>
+                    <li><strong>{number}</strong></li>
+                  </div>
+                ))
+              }
+            </ul>
+          </div> :
+          <span></span>
+        }
+      </div>
+      <div className="footer">
+        <span className="footer-text">Desenvolvido por Ezequiel Burg</span>
+      </div>
     </>
   )
 }
+
+export default App;
